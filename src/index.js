e89a5dfada8457248b1288f6a0527105e083f269
Aishik Tokdar <aishiktokdar.xyz@gmail.com>
2024-11-02 18:32:55 +0530

    Prepare for transition to Reactjs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css'; // Make sure this path is correct
+import App from './App'; // Import the App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
